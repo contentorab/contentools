1616be3feb2fe84606bd5be24a51529fe8df4d22
JohanWiren <johanwiren1@gmail.com>
2023-04-18 22:54:23 +0200

    Better filter and styling

--- extension/content.js
+++ extension/content.js
@@ -12,6 +12,14 @@ const storage = {
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.type === "job-count-words") {
     getWordCountToTranslate();
+  } else if (request.type === "job-count-words-reference") {
+    const reference = document
+      .querySelector("#_imgNotesInfo")
+      .parentElement.querySelector("span").innerText;
+    chrome.runtime.sendMessage({
+      type: "job-count-words-reference",
+      payload: reference,
+    });
   }
 });
 
@@ -20,6 +28,17 @@ function sleep(ms) {
 }
 
 async function getWordCountToTranslate() {
+  const notOnJobTab =
+    document.querySelector("a.rtsLink.rtsSelected").innerText !== "5. Jobs";
+
+  if (notOnJobTab) {
+    chrome.runtime.sendMessage({
+      type: "job-count-words-wrong-page",
+    });
+    document.querySelector(".rtsLI:nth-child(5) > a").click();
+    return;
+  }
+
   const extracted = [];
 
   for (const row of document.querySelectorAll("#divFilterResults tbody > tr")) {
@@ -28,54 +47,61 @@ async function getWordCountToTranslate() {
         ".RadToolTip.RadToolTip_Telerik .rtWrapper .rtWrapperContent > div > div"
       )
       ?.remove();
-    if (Number(row.querySelector("td:nth-child(7)").innerText) > 0) {
-      row.querySelector("td:nth-child(8) > img:last-child").click();
-      let popupLoaded = null;
-      while (!popupLoaded) {
-        await sleep(10);
-        popupLoaded = !!document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(3) tr > td:last-child"
-        );
-      }
-      const titleMatch =
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(2) td:last-child"
-        ) ||
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table td:last-child"
-        );
-      const jobTitle = titleMatch.innerText;
-      const totalWordsTd =
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(4) tr > td:last-child"
-        ) ||
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(3) tr > td:last-child"
-        );
-      const totalWords = Number(
-        totalWordsTd.innerText.match(/\((\d+) \w*\)/)[1]
-      );
-      const toTranslateMatchTd =
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(4) tr:nth-child(4) > td:last-child"
-        ) ||
-        document.querySelector(
-          ".RadToolTip .rtWrapperContent table:nth-child(3) tr:nth-child(4) > td:last-child"
-        );
-      const toTranslateMatch =
-        toTranslateMatchTd.innerText.match(/\((\d+) \w*\)/);
-      const totalWordsToTranslate = toTranslateMatch
-        ? Number(toTranslateMatch[1])
-        : 0;
-      const wordDiffCount = totalWords - totalWordsToTranslate;
-
-      extracted.push({
-        job: jobTitle,
-        total: totalWords,
-        toTranslate: totalWordsToTranslate,
-        diff: wordDiffCount,
-      });
+    if (row.querySelector("td:nth-child(7)").innerText == 0) {
+      continue;
     }
+
+    const paragraphSpans = row.querySelectorAll("td:nth-child(3) > span");
+    const paragraphs =
+      paragraphSpans.length > 2 ? paragraphSpans[1].innerText : "";
+
+    const supplierMatch = row.querySelectorAll("td:nth-child(5) > a");
+    const supplier = `${supplierMatch[0].innerText} - ${
+      supplierMatch[1].innerText ? "accepted" : ""
+    }`;
+
+    row.querySelector("td:nth-child(8) > img:last-child").click();
+    let popupLoaded = null;
+
+    while (!popupLoaded) {
+      await sleep(10);
+      popupLoaded = !!document.querySelector(
+        ".RadToolTip .rtWrapperContent table:nth-child(3) tr > td:last-child"
+      );
+    }
+
+    const titleHtml = row.querySelector("td:nth-child(4)").innerHTML;
+
+    const totalWordsTd =
+      document.querySelector(
+        ".RadToolTip .rtWrapperContent table:nth-child(4) tr > td:last-child"
+      ) ||
+      document.querySelector(
+        ".RadToolTip .rtWrapperContent table:nth-child(3) tr > td:last-child"
+      );
+    const totalWords = Number(totalWordsTd.innerText.match(/\((\d+) \w*\)/)[1]);
+    const toTranslateMatchTd =
+      document.querySelector(
+        ".RadToolTip .rtWrapperContent table:nth-child(4) tr:nth-child(4) > td:last-child"
+      ) ||
+      document.querySelector(
+        ".RadToolTip .rtWrapperContent table:nth-child(3) tr:nth-child(4) > td:last-child"
+      );
+    const toTranslateMatch =
+      toTranslateMatchTd.innerText.match(/\((\d+) \w*\)/);
+    const totalWordsToTranslate = toTranslateMatch
+      ? Number(toTranslateMatch[1])
+      : 0;
+    const wordDiffCount = totalWords - totalWordsToTranslate;
+
+    extracted.push({
+      job: titleHtml,
+      total: totalWords,
+      toTranslate: totalWordsToTranslate,
+      diff: wordDiffCount,
+      paragraphs,
+      supplier,
+    });
   }
 
   chrome.runtime.sendMessage({
@@ -83,10 +109,16 @@ async function getWordCountToTranslate() {
     payload: extracted,
   });
 
-  document.querySelector(
-    ".RadToolTip.RadToolTip_Telerik .rtWrapper .rtWrapperContent"
-  ).innerHTML = `
-    <div style="margin-top: 50px; display: flex; justify-content: center; align-items: center;">
-      <h2>Extract complete</h2>
-    </div>`;
+  try {
+    document.querySelector(
+      ".RadToolTip.RadToolTip_Telerik .rtWrapper .rtWrapperContent"
+    ).innerHTML = `
+      <div style="margin-top: 50px; display: flex; justify-content: center; align-items: center;">
+      <h2>Calculation complete</h2>
+      </div>`;
+  } catch {
+    chrome.runtime.sendMessage({
+      type: "job-count-words-wrong-page",
+    });
+  }
 }
--- src/app.tsx
+++ src/app.tsx
@@ -21,14 +21,37 @@ interface IJobWordCount {
   total: number;
   toTranslate: number;
   diff: number;
+  paragraphs: string;
+  supplier: string;
 }
+
+let isCalculatingJobs = false
 
 export function App() {
   const tabId = Number(new URLSearchParams(location.search).get('tabId'));
   const contentResponse = useSignal<IJobWordCount[]>([]);
   const filterJob = useSignal("");
+  const error = useSignal("");
+  const reference = useSignal("");
 
-  const filteredContent = useComputed(() => contentResponse.value.filter(({job}) => job.toLocaleLowerCase().includes(filterJob.value.toLocaleLowerCase())))
+  const searchFilter = (text: string, term: string) => text.toLocaleLowerCase().includes(term.toLocaleLowerCase())
+
+  const filteredContent = useComputed(() => contentResponse.value.filter(({job, supplier}) => {
+
+    const negativeSearch = filterJob.value.startsWith("-");
+    const searchWords = negativeSearch 
+      ? filterJob.value.slice(1).trim().split(" ") 
+      : filterJob.value.trim().split(" ");
+    
+    const matchJob = negativeSearch 
+      ? searchWords.filter(Boolean).some(word => searchFilter(job, word)) 
+      : searchWords.filter(Boolean).every(word => searchFilter(job, word));
+    const matchSupplier = searchFilter(supplier, searchWords[0]);
+
+    return negativeSearch 
+      ? (searchWords.filter(Boolean).length === 0 || (!matchJob && !matchSupplier)) 
+      : (matchJob || matchSupplier)
+  }))
 
   const ContentList = () => {
     const clickHandler: JSXInternal.MouseEventHandler<HTMLDivElement> = e => {
@@ -38,12 +61,14 @@ export function App() {
     }
     return (
       <div onClick={clickHandler}>
-        {filteredContent.value.map(({job, total, toTranslate, diff}: IJobWordCount) => (
+        {filteredContent.value.map(({job, total, toTranslate, diff, paragraphs, supplier}: IJobWordCount) => (
           <div class="extracted-word-count">
-            <h3>{job}</h3>
+            <h3 dangerouslySetInnerHTML={{ __html: job}}></h3>
             <p><span>Total words:</span><span title="copy" class="copy">{total}</span></p>
             <p><span>Total words to translate:</span><span title="copy" class="copy">{toTranslate}</span></p>
             <p><span>Word diff:</span><span title="copy" class="copy">{diff}</span></p>
+            <p class="blue">{paragraphs}</p>
+            <p><span>Supplier:</span><span class="violet">{supplier}</span></p>
           </div>
         ))}
       </div>
@@ -54,9 +79,25 @@ export function App() {
     chrome.tabs.sendMessage(tabId, data)
   }
 
+  sendMessageToTab({
+    type: "job-count-words-reference"
+  })
+
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "job-count-words") {
-      contentResponse.value = request.payload
+      contentResponse.value = request.payload;
+      isCalculatingJobs = false;
+    }
+    else if (request.type === "job-count-words-reference") {
+      reference.value = request.payload;
+    }
+    else if (request.type === "job-count-words-wrong-page") {
+      setTimeout(() => {
+        sendMessageToTab({
+          type: "job-count-words"
+        });
+      }, 1000);
+      isCalculatingJobs = true;
     }
   });
 
@@ -70,30 +111,34 @@ export function App() {
       <h1 style={{
         minWidth: "250px",
         textAlign: "center",
-        marginTop: "10px"
+        marginTop: "10px",
+        marginBottom: 0,
       }}>Contentools</h1>
       <div style={{
-        display: "flex",
-        justifyContent: "space-between"
+        height: "1.5rem",
+        textAlign: "center",
+        color: "gray"
+      }}>{reference}</div>
+      <button style={{ width: "100%" }} onClick={async () => {
+        error.value = ""
+        if (isCalculatingJobs) return;
+        sendMessageToTab({
+          type: "job-count-words"
+        });
+        isCalculatingJobs = true;
       }}>
-        <div>
-          <h3>Extract word count</h3>
-          <button onClick={async () => { 
-            sendMessageToTab({
-              type: "job-count-words"
-            });
-          }}>
-            Extract jobs word count
-          </button>
-        </div>
-      </div>
+        Calculate jobs word count
+      </button>
       {contentResponse.value.length > 0 ? (
         <div style={{position: "sticky", backgroundColor: "white", zIndex: 5, top: 0, padding: "0.5rem 0"}}>
-          <input value={filterJob.value} onInput={(e) => { 
+          <input value={filterJob.value} type="search" onInput={(e) => { 
             filterJob.value = (e.target as HTMLInputElement).value
-          }} type="text" placeholder="filter job" style={{width: "100%"}} />
+          }} placeholder="filter job" style={{width: "100%"}} />
         </div>
       ) : ""}
+      {error.value && <p style={{margin: "0.5rem 0", textAlign: "center"}}>
+        <strong class="red">{error}</strong>
+      </p>}
       <p>
         <ContentList />
       </p>
